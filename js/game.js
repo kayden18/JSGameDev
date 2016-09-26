0be3d9d85ede30bec345b53359a7ae131250b969
Kayden Williams <kayden18@ymail.com>
2016-09-26 11:54:16 -0600

    grr

--- js/game.js
+++ js/game.js
@@ -10,14 +10,27 @@ var mainPlayer = {
 }
 
 mainPlayer.name = prompt("Enter your name");
+
 alert("Welcome " = mainPlayer.name);
 
 var phrases = [
     prompt("Please enter mother's maiden name."),
     prompt("Enter your phone number."),
-    prompt("Enter your credit card number")
+    prompt("Enter your credit card number"),
+    prompt("What is the name of your first child?"),
 ];
 
-for (var i = 0; i < phrases.length; i++) {
+for (var i = 0 ; i < phrases.length ; i++) {
     mainPlayer.messenger(phrases[i]);
 }
+
+function PlayGame(firstNum, secondNum) {
+    return firstNum + secondNum;
+}
+
+var firstPrompt = parseInt(prompt("Enter a number between 0 and 10."));
+var secondPompt = parseInt( prompt("Enter a number between 11 and 20."))
+
+var score = PlayGame(firstPrompt, secondPompt)
+alert("Your score is " + score + ".");
+alert("Do you want to play again?");
